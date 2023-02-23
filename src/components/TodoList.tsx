6985d3c35ae89173b0bdddf6c51cf2668c216617
Amit Kapoor <amit.kapoor27@gmail.com>
2023-02-23 12:51:01 +0530

    modified:   src/components/TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,25 +13,52 @@ interface TodoListProps {
   
 }
 
-
 const TodoList = (props:TodoListProps) => {
   const [value, setValue] = React.useState<Dayjs | null>(
     dayjs('2018-01-01T00:00:00.000Z'),
   );
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState<string[]>([]);
+  const [delete_task, setdelete_task] = useState('');
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://172.16.3.48/New-SSO-b4/get_task.php');
-        const json = await response.json();
-          setTasks(JSON.parse(json));
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchData();
-  }, [tasks]);
+  }, [newTask, delete_task]);
+  const fetchData = async () => {
+    try {
+      const monthIndex = new Date(Date.parse(props.month + ` ${props.day}, ${props.year}`)).getMonth(); 
+      var month = "";
+      var day = "";
+      if(monthIndex <= 9){
+        month = `0${monthIndex+1}`
+      }else{
+        month = `${monthIndex+1}`
+      }
+  
+      const props_data = {"date":`${props.year}-${month}-${props.day}`};
+      // console.log(props_data); 
+      const response = await fetch('http://172.16.3.48/New-SSO-b4/get_task.php',{
+        method: 'POST',
+        mode: "cors",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(props_data),
+      });
+      const json = await response.json();
+        console.log(json);
+        //setJson_data(json);
+        
+        const new_arr = json.data.map((element:any, index:any) => {
+           return `${element.id} ${element.task} ${element.date_of_time}`;
+         })
+        //  console.log(new_arr);
+        setTasks(new_arr);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+
   const handleNewTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTask(event.target.value);
   };
@@ -39,13 +66,22 @@ const TodoList = (props:TodoListProps) => {
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
       let val=(value!==null)?value.format("hh:mm a"):'';
+      const monthIndex = new Date(Date.parse(props.month + ` ${props.day}, ${props.year}`)).getMonth(); 
+        var month = "";
+        var day = "";
+        if(monthIndex <= 9){
+          month = `0${monthIndex+1}`
+        }else{
+          month = `${monthIndex+1}`
+        }
+    
       let date = props.day+"-"+props.month+"-"+props.year;
       setTasks([...tasks, newTask.trim()+" | "+date+" "+ val]);
       props.onhandleSetTask();
-      let datetime= props.year+"-"+props.month+"-"+props.day+" "+((value!==null)?value.format("HH:MM:SS"):'00:00:00');
+      let datetime= props.year+"-"+month+"-"+props.day+" "+((value!==null)?value.format("HH:MM:00"):'00:00:00');
     
      handleSubmit({task: newTask.trim(),dateoftime: datetime});
-      setNewTask('');
+      
     }
   };
 
@@ -55,20 +91,25 @@ const TodoList = (props:TodoListProps) => {
     
     try {
       const obj={id:index};
-      const response = await fetch('http://172.16.3.48/New-SSO-b4/create_task.php', {
+      const response = await fetch('http://172.16.3.48/New-SSO-b4/delete_task.php', {
         method: 'POST',
+        mode: "cors",
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(obj),
       });
+
+      const json = await response.json();
+      console.log(json)
+      setdelete_task(json)
       
-      if (response.ok) {
-        // handle successful response
+      // if (response.ok) {
+      //   // handle successful response
         
-      } else {
-        // handle error response
-      }
+      // } else {
+      //   // handle error response
+      // }
     } catch (error) {
       // handle network error
     }
@@ -86,12 +127,16 @@ const TodoList = (props:TodoListProps) => {
         body: JSON.stringify(obj),
       });
       
-      if (response.ok) {
-        // handle successful response
+      const json = await response.json();
+      fetchData();
+      //console.log(json)
+      // if (response.ok) {
+      //   // handle successful response
         
-      } else {
-        // handle error response
-      }
+        
+      // } else {
+      //   // handle error response
+      // }
     } catch (error) {
       // handle network error
     }
@@ -99,15 +144,20 @@ const TodoList = (props:TodoListProps) => {
   return (
     <div>
       <List>
-        {tasks.map((task, index) => (
+        {tasks.map((task, index) => {
+          // console.log(tasks)
+          var list_name = tasks[index].split(' ')
+          var id = +list_name[0];
+          return(
           <ListItem key={index}>
-            <ListItemText primary={task} />
-            <IconButton aria-label="delete"  color="secondary" onClick={() => handleDeleteTask(index)}>
+            <ListItemText primary={`${list_name[1]} ${list_name[2]} ${list_name[3]}`} />
+            <IconButton aria-label="delete"  color="secondary" onClick={() => handleDeleteTask(id)}>
               <DeleteIcon  />
             </IconButton>
 
           </ListItem>
-        ))}
+        )
+        })}
       </List>
       <FormControl  sx={{display: 'flex',alignItems: 'center','& .MuiTextField-root': { m: 1, width: '25ch',paddingBottom:'2px' }}}>
         <TextField
